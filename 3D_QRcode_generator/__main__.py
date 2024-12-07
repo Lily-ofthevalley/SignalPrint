1bd470d76fc35787de897f2bd5fc5b1a5ef7a643
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-12-07 15:13:07 +0100

    Update __main__.py

--- 3D_QRcode_generator/__main__.py
+++ 3D_QRcode_generator/__main__.py
@@ -1,9 +1,11 @@
 from qr_generator import generate_qr_code
+from model_generator import qr_to_stl
 
 
 def main():
     if __name__ == '__main__':
         generate_qr_code()
+        qr_to_stl()
 
 
 main()
