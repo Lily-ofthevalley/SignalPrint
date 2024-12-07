d1f3d49a1fd7c9a0b69765a6222406da87464d00
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-12-07 12:50:32 +0100

    Generate qrcode image

--- 3D_QRcode_generator/qr_generator.py
+++ 3D_QRcode_generator/qr_generator.py
@@ -1,2 +1,20 @@
 import qrcode
+from pathlib import Path
+import qrcode.constants
 from utils import GetWifiDetails
+
+
+def GenerateQrCode():
+    wifi_data = GetWifiDetails()
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=20,
+        border=1
+    )
+    qr.add_data(wifi_data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("./Qrcode/QrCode.png", "PNG")
