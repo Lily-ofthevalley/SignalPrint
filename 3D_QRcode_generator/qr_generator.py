efbe539b4aea3c82771b834b6fa69a30236de79f
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-11 11:54:24 +0100

    made image smaller

--- 3D_QRcode_generator/qr_generator.py
+++ 3D_QRcode_generator/qr_generator.py
@@ -8,9 +8,9 @@ def generate_qr_code():
     wifi_data = get_wifi_details()
 
     qr = qrcode.QRCode(
-        version=1,
+        version=2,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=20,
+        box_size=1,
         border=1
     )
     qr.add_data(wifi_data)
