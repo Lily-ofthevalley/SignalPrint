39b03e91f6833e384607073a9be99017ecbc1c50
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-18 12:21:14 +0100

    align meshes correctly

--- 3D_QRcode_generator/model_generator.py
+++ 3D_QRcode_generator/model_generator.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from stl import mesh
 from PIL import Image
+from math import cos, sin, radians
 from utils import get_downloads_folder
 
 
@@ -10,7 +11,7 @@ def qr_to_stl():
     img_array = np.array(img)
 
     cube_size = 1.0 #Size of each cube
-    cube_height = 2.5 #Height og each cube
+    cube_height = 1.5 #Height og each cube
     scale_factor = 1 #Scaling factor to reduce size
 
     vertices = []
@@ -56,9 +57,57 @@ def qr_to_stl():
 
     qr_mesh.save('./supportingFiles/qrcode_3d.stl')
 
+def rotate_mesh(vertices, angle_degrees, axis):
+    angle_radians = radians(angle_degrees)
+    if axis == 0:  # Rotate around x-axis
+        rotation_matrix = np.array([
+            [1, 0, 0],
+            [0, cos(angle_radians), -sin(angle_radians)],
+            [0, sin(angle_radians), cos(angle_radians)]
+        ])
+    elif axis == 1:  # Rotate around y-axis
+        rotation_matrix = np.array([
+            [cos(angle_radians), 0, sin(angle_radians)],
+            [0, 1, 0],
+            [-sin(angle_radians), 0, cos(angle_radians)]
+        ])
+    elif axis == 2:  # Rotate around z-axis
+        rotation_matrix = np.array([
+            [cos(angle_radians), -sin(angle_radians), 0],
+            [sin(angle_radians), cos(angle_radians), 0],
+            [0, 0, 1]
+        ])
+    return np.dot(vertices, rotation_matrix.T)
+
+def translate_mesh(vertices, translation):
+    return vertices + translation
+
+def center_mesh(mesh):
+    min_coords = np.min(mesh.vectors, axis=(0, 1))
+    max_coords = np.max(mesh.vectors, axis=(0, 1))
+    center = (min_coords + max_coords) / 2.0
+    
+    translation = -center
+    return translate_mesh(mesh.vectors, translation)
+
 def combine_stl_files(file1, file2):
-    mesh1 = mesh.Mesh.from_file(file1)
-    mesh2 = mesh.Mesh.from_file(file2)
+    mesh1 = mesh.Mesh.from_file(file1) # The QrCode
+    mesh2 = mesh.Mesh.from_file(file2) # The sign
+
+    # Center both meshes at the origin
+    mesh1.vectors = center_mesh(mesh1)
+    mesh2.vectors = center_mesh(mesh2)
+
+
+    translation1 = np.array([0, 10, 4.5])  # Move mesh1 10 units along the y-axis and 4.5 units along the z-axis
+    rotation_x = (78, 0)  # Rotate mesh1 78 degrees around the x-axis
+
+
+     # Apply transformations to mesh1
+    mesh1.vectors = translate_mesh(mesh1.vectors, translation1)
+    angle, axis = rotation_x
+    mesh1.vectors = rotate_mesh(mesh1.vectors, angle, axis)
+
     combined_vertices = np.vstack((mesh1.vectors, mesh2.vectors))
     
     combined_mesh = mesh.Mesh(np.zeros(combined_vertices.shape[0], dtype=mesh.Mesh.dtype))
