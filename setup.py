b5bb40716cf2c75595a6dba470be2bebfd15ab6d
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-18 17:59:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_cli_project',
-    version='0.1',
+    name='SignalPrint',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
-        'click',
-        'colorama',
-        'euclid3',
-        'numpy',
-        'numpy-stl',
-        'pillow',
-        'ply',
-        'prettytable',
-        'pypng',
-        'python-utils',
-        'qrcode[pil]',
-        'setuptools',
-        'solidpython',
-        'typing-extensions',
+        'click>=8.0.0',
+        'numpy>=1.21.0',
+        'Pillow>=8.0.0',
+        'qrcode>=7.0.0',
+        'numpy-stl>=2.16.0',
     ],
     entry_points={
         'console_scripts': [
-            'SignalPrint=SignalPrint.cli:cli',
+            'SignalPrint=SignalPrint.cli:cli',  # Command-line entry point
         ],
     },
+    include_package_data=True,
+    package_data={
+        'SignalPrint': ['supportingFiles/*'],
+    },
+    python_requires='>=3.6',
 )
