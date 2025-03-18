a03862d6511005c236110918593d15925d2307e3
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-18 17:56:24 +0100

    added third option

--- cli.py
+++ cli.py
@@ -15,21 +15,25 @@ def show_start_screen():
     click.echo("Welcome to the SignalPrint CLI Tool!")
     click.echo("====================================")
     click.echo("\nPlease choose an option:")
-    click.echo("1. Generate QR Code and STL")
-    click.echo("2. Exit")
+    click.echo("1. Automatically Generate QR Code and STL")
+    click.echo("2. Generate QR Code and STL for Custom Wifi (COMING SOON)")
+    click.echo("3. Exit")
 
     while True:
-        choice = click.prompt("Enter your choice (1 or 2)", type=int)
+        choice = click.prompt("Enter your choice (1, 2 or 3)", type=int)
 
         if choice == 1:
             click.echo("\nYou selected: Generate QR Code and STL")
-            generate()  # Call the generate command
+            generate()
             break
         elif choice == 2:
             click.echo("\nExiting the program. Goodbye!")
             break
+        elif choice == 3:
+            click.echo("\nExiting the program. Goodbye!")
+            break
         else:
-            click.echo("Invalid choice. Please enter 1 or 2.")
+            click.echo("Invalid choice. Please enter 1, 2 or 3.")
 
 if __name__ == "__main__":
     show_start_screen()
