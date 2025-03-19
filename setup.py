29f2d17d547283261a7df9b25f3eb1fab86f7ce9
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-19 10:03:54 +0100

    changed qrcode to qrcode[pil]

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
         'click>=8.0.0',
         'numpy>=1.21.0',
         'Pillow>=8.0.0',
-        'qrcode>=7.0.0',
+        'qrcode[pil]>=7.0.0',
         'numpy-stl>=2.16.0',
     ],
     entry_points={
         'console_scripts': [
-            'SignalPrint=SignalPrint.cli:cli',  # Command-line entry point
+            'SignalPrint=SignalPrint.cli:cli',
         ],
     },
     include_package_data=True,
